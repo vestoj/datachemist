f4eea76feda90a2ae33aefd1a5127829071cebca
James Vesto <jamesvesto@gmail.com>
2020-06-25 02:33:29 -0500

    resolved a bug in the package DESCRIPTION, and documented the calibrate function

--- R/calibrate.R
+++ R/calibrate.R
@@ -1,8 +1,17 @@
-#This function will calibrate data from an instrument (example: ICPMS)
-#(weighted linear calibration)
-#signal, RSD, conc should be columns of data (ex. ICPMS$signal)
-#element should be what the instrument is testing for the concentration of as a character (ex. "Pb")
-#units should be reported as characters (ex. "(ppb)")
+#' Weighted Linear Calibration from instrumental data
+#'
+#' @param signal attribute
+#' @param RSD attribute
+#' @param conc attribute
+#' @param element character
+#' @param conc_unit character
+#' @param signal_unit character
+#'
+#' @return
+#' @export
+#'
+#' @examples calibrate(ICPMS$signal, ICPMS$RSD, ICPMS$conc, "Pb", "um", "ppb")
+#'
 
 calibrate <- function(signal, RSD, conc, element, conc_unit, signal_unit){
   w <- 1/(signal*RSD)^2
@@ -20,7 +29,15 @@ calibrate <- function(signal, RSD, conc, element, conc_unit, signal_unit){
     graphics::title( paste("Calibration for", element))
 
   cat("Concentration", conc_unit, "=", slope, stringi::stri_escape_unicode("\u241"), slope_STD,"(x) + (", intercept, stringi::stri_escape_unicode("\u241"), intercept_STD,")")
-  equation <- data.frame(element, slope, slope_STD, intercept, intercept_STD)}
+  equation <- data.frame(element, slope, slope_STD, intercept, intercept_STD)
+  }
+
+
+#This function will calibrate data from an instrument (example: ICPMS)
+#(weighted linear calibration)
+#signal, RSD, conc should be columns of data (ex. ICPMS$signal)
+#element should be what the instrument is testing for the concentration of as a character (ex. "Pb")
+#units should be reported as characters (ex. "(ppb)")
 
 #function returns a data frame that lists the element, slope, slope_STD, intercept, and intercept_STD
 #function also plots a linear calibration curve
